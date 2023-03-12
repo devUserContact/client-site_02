fd6fd5cec1aac0da2cd7ce89faf7809c08fc855b
devUserContact <developer.user.contact@protonmail.com>
2023-03-11 22:47:08 -0800

    added assets directory

---
+++ assets/MediaLinks.ts
@@ -0,0 +1,10 @@
+export interface MediaLinks {
+  platform: string
+  url: string
+}
+export const mediaLinks: MediaLinks[] = [
+  {
+    platform: 'spotify',
+    url: `"<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/2FmT7TQ2nAMhahAB9CkXYr?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>"`
+  },
+]
