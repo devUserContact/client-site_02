d3456b8a347abbe8b47069eda6dfb0110b0432db
devUserContact <developer.user.contact@protonmail.com>
2023-03-13 21:11:06 -0700

    implemented embeded spotify player

--- components/LinkList/index.tsx
+++ components/LinkList/index.tsx
@@ -1,16 +1,27 @@
 import React from 'react'
 import styles from '../../styles/Main.module.scss'
+import { mediaLinks } from '../../assets/MediaLinks'
 
 const LinkList = () => {
   return (
     <>
+      <div>
+        {mediaLinks.map((mediaLinks: any) => {
+          if (mediaLinks.platform === 'spotify') {
+            return <div dangerouslySetInnerHTML={{ __html: mediaLinks.url }}></div>
+          }
+        })}
+      </div>
       <div className={styles.linkListContainer}>
-          <a className={styles.linkList} href='https://cranesong.bandcamp.com/music'>
-            bandcamp
-          </a>
-          <a className={styles.linkList} href='https://soundcloud.com'>
-            soundcloud
-          </a>
+        <a
+          className={styles.linkList}
+          href='https://cranesong.bandcamp.com/music'
+        >
+          bandcamp
+        </a>
+        <a className={styles.linkList} href='https://soundcloud.com'>
+          soundcloud
+        </a>
       </div>
     </>
   )
