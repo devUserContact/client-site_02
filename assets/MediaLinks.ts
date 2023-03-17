9c43bd41ffa0b8860830659e81e306fc5f158e55
devUserContact <developer.user.contact@protonmail.com>
2023-03-16 20:23:22 -0700

    edited MediaLinks interface

--- assets/MediaLinks.ts
+++ assets/MediaLinks.ts
@@ -1,10 +1,11 @@
 export interface MediaLinks {
   platform: string
-  url: string
+  url?: string
+  html?: string
 }
 export const mediaLinks: MediaLinks[] = [
   {
     platform: 'spotify',
-    url: `"<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/2FmT7TQ2nAMhahAB9CkXYr?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>"`
+    html: `"<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/2FmT7TQ2nAMhahAB9CkXYr?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>"`,
   },
 ]
