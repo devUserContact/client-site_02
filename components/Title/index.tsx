ad234f4dcd4b4f83c9196724067aa687319977cd
devUserContct <developer.user.contact@protonmail.com>
2023-04-21 23:35:24 -0700

    implementing css styled landing page

--- components/Title/index.tsx
+++ components/Title/index.tsx
@@ -6,7 +6,13 @@ const Title = () => {
     <>
       <div className={styles.mainContainer}>
         <div className={styles.textContainer}>
-          <p className={styles.titleTextLarge}>Crane Song</p>
+          <div id='circle' style={{width: "4em", height: "8em"}}>
+            <div style={{transform: "translate3d(0em, 0em, 0em)"}}>C</div>
+            <div style={{transform: "translate3d(0em, 0em, 0em)"}}>R</div>
+            <div style={{transform: "translate3d(0em, 0em, 0em)"}}>A</div>
+            <div style={{transform: "translate3d(0em, 0em, 0em)"}}>N</div>
+            <div style={{transform: "translate3d(0em, 0em, 0em)"}}>E</div>
+          </div>
           <p className={styles.titleTextSmall}>a singer songwriter from CA</p>
         </div>
       </div>
